a013c5ef9a9e9b19f8acf486eb92ac2b73d32d45
Bender <danielbendersantos@gmail.com>
2023-09-12 16:43:16 -0300

    SharedPreferences Limpar dados

--- AppListaCurso/app/src/main/java/devandroid/bender/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/bender/applistacurso/view/MainActivity.java
@@ -17,6 +17,7 @@ import devandroid.bender.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+    SharedPreferences.Editor listaVip ;
     public static final String NOME_PREFERENCES = "pref_listavip";
     PessoaController controller;
     Pessoa pessoa;
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preferences = getSharedPreferences(NOME_PREFERENCES,0);
-        SharedPreferences.Editor listaVip = preferences.edit();
+        listaVip = preferences.edit();
 
         controller = new PessoaController();
         controller.toString();
@@ -68,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
                 editSobreNome.setText("");
                 editNomeCurso.setText("");
                 editTelefoneContato.setText("");
+
+                listaVip.clear();
+                listaVip.apply();
             }
         });
 
