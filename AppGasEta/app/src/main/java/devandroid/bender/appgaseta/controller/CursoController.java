6da5ae8cc41680816b4e40aa7d3b50df63621864
Bender <danielbendersantos@gmail.com>
2023-09-16 16:14:43 -0300

    AppGasEta com layout e calculo fixo

---
+++ AppGasEta/app/src/main/java/devandroid/bender/appgaseta/controller/CursoController.java
@@ -0,0 +1,29 @@
+package devandroid.bender.appgaseta.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import devandroid.bender.appgaseta.model.Curso;
+
+public class CursoController {
+    private List listCursos;
+    public List getListaDeCursos(){
+        listCursos = new ArrayList<Curso>();
+        listCursos.add(new Curso("Java"));
+        listCursos.add(new Curso("HTML"));
+        listCursos.add(new Curso("C#"));
+
+        return listCursos;
+    }
+
+    public ArrayList<String>dadosParaSpinner(){
+        ArrayList<String> dados = new ArrayList<>();
+
+        for (int i = 0; i < getListaDeCursos().size(); i++) {
+            Curso obejeto = (Curso) getListaDeCursos().get(i);
+            dados.add(obejeto.getNomeDoCursoDesejado());
+        }
+
+        return dados ;
+    }
+}
---
+++ AppGasEta/app/src/main/java/devandroid/bender/appgaseta/view/MainActivity.java
@@ -0,0 +1,117 @@
+package devandroid.bender.appgaseta.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.List;
+
+import devandroid.bender.appgaseta.R;
+import devandroid.bender.appgaseta.controller.CursoController;
+import devandroid.bender.appgaseta.controller.PessoaController;
+import devandroid.bender.appgaseta.model.Pessoa;
+
+public class MainActivity extends AppCompatActivity {
+
+    PessoaController controller;
+    CursoController cursoController;
+    Pessoa pessoa;
+    List<String> nomeDosCursos;
+    Pessoa outraPessoa;
+
+    EditText editPrimeiroNome;
+    EditText editSobreNome;
+    EditText editNomeCurso;
+    EditText editTelefoneContato;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner);
+
+        controller = new PessoaController(MainActivity.this);
+        controller.toString();
+
+
+
+        pessoa = new Pessoa();
+        controller.buscar(pessoa);
+
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobreNome = findViewById(R.id.editSobreNome);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobreNome.setText(pessoa.getSobreNome());
+        editNomeCurso.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        //adapter
+        //layout
+        //injetar o adapter ou spinner - a lista gerada
+
+        cursoController = new CursoController();
+        nomeDosCursos = cursoController.dadosParaSpinner();
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+        cursoController.dadosParaSpinner());
+
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+
+        spinner.setAdapter(adapter);
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editPrimeiroNome.setText("");
+                editSobreNome.setText("");
+                editNomeCurso.setText("");
+                editTelefoneContato.setText("");
+
+                controller.limpar();
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte Sempre", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobreNome(editSobreNome.getText().toString());
+                pessoa.setCursoDesejado(editNomeCurso.getText().toString());
+                pessoa.setTelefoneContato(editTelefoneContato.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo " + pessoa.toString(), Toast.LENGTH_LONG).show();
+
+                controller.salvar(pessoa);
+            }
+        });
+
+        Log.i("POOAndroid", pessoa.toString());
+    }
+}
